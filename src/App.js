a0ed42fbd1e38a77e5a850fea347437360b1c4a1
Nick Gray <nick.gray@rea-group.com>
2019-04-08 08:48:36 +1000

    WIP: Spotify auth through context and hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, {Component} from 'react';
-import axios from 'axios'
-// import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import Dashboard from "./components/Dashboard";
 import {SpotifyProvider} from "./contexts/SpotifyContext";
-import SignIn from "./components/SignIn";
 
 class App extends Component {
   render() {
     return (
-        <SpotifyProvider>
-          <div className="App">
-            {/*<button onClick={() => this.spotifyAuth()}>Sign in</button>*/}
-            <div className="header">
-              <SignIn></SignIn>
-            </div>
-          </div>
-        </SpotifyProvider>
+        <Router>
+          <SpotifyProvider>
+          <Route path="/" component={Dashboard}/>
+          </SpotifyProvider>
+        </Router>
     );
   }
 
--- src/contexts/SpotifyContext.js
+++ src/contexts/SpotifyContext.js
@@ -5,18 +5,43 @@ const SpotifyContext = React.createContext();
 
 class SpotifyProvider extends Component {
   state = {
-    something: "testing"
+    loggedIn: false,
+    accessToken: null,
+    user: null
   };
 
   render() {
     return (
         <SpotifyContext.Provider value={{
           state: this.state,
-          performAuth: this.spotifyAuth
+          performAuth: this.spotifyAuth,
+          setUser: this.setUser,
+          setAccessToken: (token) => this.setAccessToken(token)
         }}>
           {this.props.children}
         </SpotifyContext.Provider>
     )
+  }
+
+  setUser() {
+    axios.get('https://api.spotify.com/v1/me', {
+      headers: {
+        'Authorization': 'Bearer ' + this.state.accessToken
+      }
+    }).then(res => {
+      const {data: user} = res;
+      this.setState({user});
+
+    })
+  }
+
+  setAccessToken(token){
+    console.log("setting token");
+    this.setState({
+      accessToken: token,
+      loggedIn: true},
+        () =>  {this.setUser()
+    });
   }
 
   spotifyAuth() {
